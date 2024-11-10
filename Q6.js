dd697bc0be64d364df08672b80a38f433d2c1018
Novish Khalil <novishkhalil2@hotmail.com>
2024-11-11 00:05:12 +0500

    Add files via upload

---
+++ Q6.js
@@ -0,0 +1,8 @@
+"use strict";
+//6.Stripping Names: Store a person’s name, and include some whitespace characters at the beginning and end of the name. Make sure you use each character combination, "\t" and "\n", at least once. Print the name once, so the whitespace around the name is displayed. Then print the name after striping the white spaces.
+let prName = "NovishKhalil";
+console.log(prName);
+let prsName = "Novish\tKhalil";
+console.log(prsName);
+let pName = "Novish\nKhalil";
+console.log(pName);
